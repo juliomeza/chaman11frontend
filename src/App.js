1aa85724967febcf9f9c8a9aa95d40a1c7793fea
Julio Meza <juliomeza@gmail.com>
2023-09-26 20:41:56 -0400

    Initial commit Frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [userMessage, setUserMessage] = useState('');
+  const [chatbotMessage, setChatbotMessage] = useState('');
+
+  const backendUrl = process.env.REACT_APP_BACKEND_URL
+
+
+
+  const sendMessage = async (event) => {
+    try {
+      const response = await fetch(`${backendUrl}/chat`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ user_message: userMessage }),
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(data || "Something went wrong");
+      }
+
+      setChatbotMessage(data.chatbot_response);
+    }
+    catch (error) {
+      console.error(error);
+      setChatbotMessage("Sorry, something went wrong")
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        value={userMessage}
+        onChange={(event) => setUserMessage(event.target.value)}
+      />
+      <button onClick={sendMessage}>Send</button>
+      <p>{chatbotMessage}</p>
     </div>
   );
 }
